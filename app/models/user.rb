df14e32f385f2904db7879d708eacb48972970eb
hnamitha1 <namitha.h@gmail.com>
2016-05-26 20:40:47 +0100

    adding images

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,10 @@ class User < ActiveRecord::Base
   has_many :reviewed_restaurants, through: :reviews, source: :restaurant
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  devise :omniauthable, :omniauth_providers => [:facebook]     
+  devise :omniauthable, :omniauth_providers => [:facebook] 
+
+  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/    
   
   def self.from_omniauth(auth)
 	  where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
